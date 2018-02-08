9f1bba736255529c9a8cd56e03a84b7887d77e4a
Xinsheng Fang <xinsfang@cisco.com>
2018-02-08 18:25:30 -0500

    add provision.sh reset()

--- shell_ubuntu_16.04.3/provision.sh
+++ shell_ubuntu_16.04.3/provision.sh
@@ -10,7 +10,6 @@ MASTER_IP="10.74.68.151"
 
 master_new()
 {
-  kubeadm reset
   #kubeadm init --apiserver-advertise-address=${MASTER_IP} --pod-network-cidr=${POD_NW_CIDR} --token ${KUBETOKEN} --token-ttl 0
   kubeadm init --config ./kubeadm.conf_master1
   
@@ -23,7 +22,12 @@ master_new()
 
 master_join()
 {
-  kubeadm reset
+  #etcd cluster
+  curl http://10.74.68.151:2379/v2/members
+  curl -X POST -H "Content-Type: application/json" -d '
+{"peerURLs": ["http://10.74.68.153:2380"]}
+'  http://10.74.68.151:2379/v2/members
+
   kubeadm init --config kubeadm.conf_master2
   
   mkdir -p $HOME/.kube
@@ -32,18 +36,22 @@ master_join()
   
   kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
-  #etcd cluster
-  curl http://10.74.68.151:2379/v2/members
-  curl -X POST -H "Content-Type: application/json" -d '
-{"peerURLs": ["http://10.74.68.153:2380"]}
-'  http://10.74.68.151:2379/v2/members
 }
 
 worker()
 {
-  kubeadm reset
   #kubeadm join --token b029ee.968a33e8d8e6bb0d 10.74.68.151:6443 --discovery-token-ca-cert-hash sha256:e5b86401e844f766347f8cc336763b37ac83317f65a1327f0728ef981f403d36
   kubeadm join --token ${KUBETOKEN} --discovery-token-unsafe-skip-ca-verification ${MASTER_IP}:6443
+}
+
+reset()
+{
+  kubeadm reset
+  ifconfig cni0 down
+  ip link delete cni0
+  ifconfig flannel.1 down
+  ip link delete flannel.1
+  rm -rf /var/lib/cni/
 }
 
 #./provision.sh master_new
